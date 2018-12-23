a90322a390fe5d49bfe1c534c98324e663b1b799
Axel <axelfuh@gmail.com>
2018-12-23 17:04:31 +0100

    Add blog name

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Gatsby personal starter
-
-- Styled components
-- Gatsby Image
-- SEO
-- Google Analytics
-- PWA (Manifest and Offline)
-- Netlify
-- Makrdown for the section's content `src/content/` and pages
-  - Responsive images and iframe
-  - Syntax highlighting
+# Talita traveler blog
--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
-  title: 'Title',
-  titleTemplate: '%s | Site title',
-  description: "Site description.",
+  title: 'Talita traveler',
+  titleTemplate: '%s | Talita traveler',
+  description: "Desde otro lado.",
   siteUrl: 'https://talitatraveler.netlify.com', // no trailing slash
   favicon: '/images/icon-32x32.png',
   image: '/images/cover-1440x720.jpg',
