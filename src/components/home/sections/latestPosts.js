34df1b55d9405221baa3eeaa3bb77d5aafd1111f
Axel <axelfuh@gmail.com>
2019-01-12 21:00:33 +0100

    Update UI

--- src/components/home/sections/latestPosts.js
+++ src/components/home/sections/latestPosts.js
@@ -1,37 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
 
-import { PostCard } from '../../../utils/UI'
+import { PostCard, MoreButton } from '../../../utils/UI'
 import { withPostCounter } from '../../../utils/context/postsToShow'
-
-const Button = styled.button`
-  width: 100px;
-  height: 100px;
-
-  border: none;
-  border-radius: 100%;
-
-  font-size: 14px;
-  font-weight: bold;
-  text-transform: uppercase;
-
-  cursor: pointer;
-
-  ${({ theme }) => theme && css`
-    background: ${theme.white};
-    box-shadow: ${theme.shadow};
-  `};
-
-  &:hover {
-    ${({ theme }) => theme && css`
-      background: ${theme.mainColor};
-      color: ${theme.white};
-    `};
-  }
-
-  transition: all .2s;
-`
 
 const LatestsPosts = ({ edges, postsToShow, onShowMorePosts }) => (
   <div>
@@ -42,11 +13,11 @@ const LatestsPosts = ({ edges, postsToShow, onShowMorePosts }) => (
       />
     ))}
     <div style={{ display: 'flex', justifyContent: 'center' }}>
-      <Button
+      <MoreButton
         onClick={onShowMorePosts}
       >
         Ver más
-      </Button>
+      </MoreButton>
     </div>
   </div>
 )
--- src/utils/UI/index.js
+++ src/utils/UI/index.js
@@ -1,3 +1,4 @@
 export { Section } from './containers'
 export { PostCard } from './postCard'
 export { SocialLinks } from './socialLinks'
+export { MoreButton } from './buttons'
