c6ebb3de8688d1bda372c980a7bd2496898530db
Axel <axelfuh@gmail.com>
2019-02-01 18:43:11 +0100

    Update post header

--- src/components/post/headerImage.js
+++ src/components/post/headerImage.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import GatsbyImg from 'gatsby-image'
-import styled from 'styled-components'
-
-const Wrapper = styled.div`
-  margin-top: 20px;
-`
 
 const headerImage = ({ post }) => (
-  <Wrapper>
-    <GatsbyImg
-      style={{ maxHeight: '84vh' }}
-      fluid={post.headerImage.fluid}
-      alt={post.headerImage.description}
-      title={post.headerImage.description}
-    />
-  </Wrapper>
+  <GatsbyImg
+    style={{ maxHeight: '84vh' }}
+    fluid={post.headerImage.fluid}
+    alt={post.headerImage.description}
+    title={post.headerImage.description}
+  />
 )
 
 headerImage.propTypes = {
