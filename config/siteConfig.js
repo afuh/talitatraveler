1cfce2cc85aa11643a17735e79fff912ab8c3920
Axel <axelfuh@gmail.com>
2019-02-03 10:50:25 +0100

    Update page structure

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,14 +1,26 @@
+const siteUrl = 'https://talitatraveler.netlify.com'
+
+const mainNav = [
+  { name: 'Inicio', path: '/' },
+  { name: "Categorías", path: '/categorias' },
+  { name: "Sobre mi", path: '/sobre-mi' }
+]
+
 module.exports = {
   title: 'Talita traveler',
   titleTemplate: '%s | Talita traveler',
   description: "Desde otro lado.",
-  siteUrl: 'https://talitatraveler.netlify.com', // no trailing slash
+  siteUrl, // no trailing slash
   favicon: '/images/icon-32x32.png',
   image: '/images/cover-1440x720.jpg',
   themeColor: '#000',
   backgroundColor: '#fff',
   nav: [
-    { name: 'Home', path: '/' },
-    { name: "Categorías", path: '/categorias' }
+    ...mainNav
+  ],
+  footerNav: [
+    ...mainNav,
+    { name: "Contacto", path: '/contacto' },
+    { name: "rss", path: siteUrl + '/rss.xml', external: true }
   ]
 }
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,58 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
 
-import ContactForm from './contactForm'
-import { media } from '../../utils/style'
-import { SocialIcon } from '../../utils/UI/icons'
-
-const Wrapper = styled.div`
-  display: flex;
-
-  ${media.phone(css`
-    display: block;
-  `)}
-`
+import { Section } from '../../utils/UI'
 
 const Description = styled.div`
   white-space: pre-line;
 `
 
-const IconsWrapper = styled.div`
-  display: flex;
-`
-
-const AboutSection = ({ author }) => (
-  <div style={{ marginRight: 20 }}>
-    <h1>{author.name}</h1>
-    <Description dangerouslySetInnerHTML={{ __html: author.about.md.html }}/><br/>
-    <p>Podés mandarme un <a href={`mailto:${author.contact.mail}`}>mail</a></p>
-    <IconsWrapper>
-      {author.contact.social.map(icon => (
-        <SocialIcon
-          key={icon.name}
-          href={icon.url}
-          name={icon.name}
-        />
-      ))}
-    </IconsWrapper>
-  </div>
-)
-
-AboutSection.propTypes = {
-  author: PropTypes.object.isRequired
-}
-
-
 const About = ({ author }) => (
-  <Wrapper>
-    <div style={{ flex: 1 }}>
-      <AboutSection author={author}/>
+  <Section margin={2}>
+    <div style={{ marginRight: 20, paddingBottom: 50 }}>
+      <h1>{author.name}</h1>
+      <Description dangerouslySetInnerHTML={{ __html: author.about.md.html }}/>
+      <Link to='/contacto'>
+        contacto
+      </Link>
     </div>
-    <div style={{ flex: 1 }}>
-      <ContactForm />
-    </div>
-  </Wrapper>
+  </Section>
 )
 
 About.propTypes = {
