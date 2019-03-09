fec30b33b83b9b37017fec5cf8083a443b87549d
Axel <axelfuh@gmail.com>
2019-03-09 16:26:30 +0100

    Add Hero

---
+++ src/components/home/hero.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useStaticQuery, graphql } from 'gatsby'
+import GatsbyImg from 'gatsby-image'
+import styled, { css } from 'styled-components'
+import { FaChevronDown } from 'react-icons/fa'
+import { scroller } from 'react-scroll'
+
+const Icon = styled(FaChevronDown)`
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.1)
+  }
+
+  transition: transform 0.2s ease-in;
+`
+
+const Wrapper = styled.div`
+  ${({ theme }) => theme && css`
+    min-height: calc(100vh - ${theme.headerHeight}px);
+  `};
+
+  display: flex;
+  flex-direction: column;
+
+  .icon-wrapper {
+    flex: 1;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    color: ${({ theme }) => theme.white};
+    font-family: 'Noto Serif KR', serif;
+    font-weight: 900;
+    font-size: 6.0rem;
+    letter-spacing: 0.1em;
+    text-align: center;
+    line-height: 0.6;
+  }
+`
+
+const HeaderImage = ({ image, children }) => (
+  <div style={{ position: 'relative' }}>
+    <GatsbyImg
+      style={{ height: 480 }}
+      fluid={image.fluid}
+      alt={image.description}
+      title={image.description}
+    />
+    <Overlay>{children}</Overlay>
+  </div>
+)
+
+HeaderImage.propTypes = {
+  image: PropTypes.object.isRequired
+}
+
+const Hero = ({ scrollId }) => {
+  const { hero: { header, image } } = useStaticQuery(query)
+
+  return (
+    <Wrapper>
+      <HeaderImage image={image}>
+        <h1>{header}</h1>
+      </HeaderImage>
+      <div className='icon-wrapper'>
+        <Icon
+          onClick={() => scroller.scrollTo(scrollId, {
+            duration: 1000,
+            smooth: "easeOutQuint",
+            offset: -20
+          })}
+        />
+      </div>
+    </Wrapper>
+  )
+}
+
+Hero.propTypes = {
+  scrollId: PropTypes.string.isRequired
+}
+
+const query = graphql`
+  query HERO_QUERY {
+    hero: contentfulHero {
+      header
+      image {
+        description
+        fluid(maxWidth: 2000) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+      }
+    }
+  }
+`
+
+export default Hero
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Hero from './hero'
 import { Section, PostsGrid } from '../../utils/UI'
+
+const scrollId = 'post-grid'
 
 const Home = ({ data: { posts: { edges, totalCount } } }) => (
   <Section>
+    <Hero scrollId={scrollId}/>
+    <h2
+      style={{
+        textAlign: 'center',
+        fontSize: '2.9rem'
+      }}
+      id={scrollId}
+    >
+      Últimos posts
+    </h2>
     <PostsGrid
       totalCount={totalCount}
       posts={edges}
